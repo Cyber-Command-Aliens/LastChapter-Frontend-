601602420b15a3163863590a842bc2858fad9290
MasteRminD6666 <ramikhzaitoun@gmail.com>
2021-09-17 20:08:06 +0300

    addıng page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+//Imports Here =) 
+import React from 'react';
+import Home from './Components/Home/Home';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//Render The Components Between the Header and The footer 
+class app extends React.Component {
+  render() {
+    return (
+      <>
+        <Header></Header>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <Home></Home>
+
+
+
+
+
+
+        <Footer></Footer>
+      </>
+    );
+  }
 }
 
-export default App;
+export default app;
