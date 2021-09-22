dcfdd2b48ca55da21d7c76889f61b8736274cd9f
MasteRminD6666 <ramikhzaitoun@gmail.com>
2021-09-22 11:24:01 +0300

    adding

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Card from 'react-bootstrap/Card'
 import { withAuth0 } from "@auth0/auth0-react";
-import { Container, Row , Col} from "react-bootstrap/";
+import { Container, Row, Col } from "react-bootstrap/";
 import BookCards from "./BookCards";
-import {HomeWrapper} from './BookCardStyle';
+import { HomeWrapper } from './BookCardStyle';
 import FilterClassics from './Classics';
 import FilterComics from './Comics';
 import FilterMystery from './Mystery';
@@ -13,28 +13,28 @@ import './Home.scss'
 
 // i should render the catagory in a single page i will do it later 
 class Home extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props)
-    this.state={
-      show:false,
+    this.state = {
+      show: false,
       title: '',
-      img:'',
-      author:'',
-      status:'',
-      pages:'',
+      img: '',
+      author: '',
+      status: '',
+      pages: '',
       infoLink: ''
     }
   }
-  handleShow =(title,img,author,status,pages,infoLink,catgory) => {
+  handleShow = (title, img, author, status, pages, infoLink, catgory) => {
     this.setState({
       show: true,
       title: title,
-      img:img,
-      author:author,
-      status:status,
-      pages:pages,
+      img: img,
+      author: author,
+      status: status,
+      pages: pages,
       infoLink: infoLink,
-      catgory:catgory
+      catgory: catgory
     })
     console.log(title);
   }
@@ -63,57 +63,127 @@ class Home extends React.Component {
       //   title: 'qwe',
       //   img: 'https://static.thriftbooks.com/general/DT-S-_264x144_83787454.jpg'
       // },
-  
+
     ]
     return (
       <>
-      <Container>
-     <Row xs={2} md={2} className="g-3">
-  {CardsArr.map((item) => (
-    <Col>
-      <Card style ={{ marginTop:'40px'}} className="Cards">
-        <Card.Img variant="top" src={item.img} />
-        <Card.Body>
-          <Card.Title>  
-            <a style={{textDecoration:'none', color:'Black'}} href="">{item.title} </a><br></br>
-            <a style={{textDecoration:'none', color:'#008575'}} href="">{item.desc} </a>
-            </Card.Title>
-       
-         
-         
-        </Card.Body>
-      </Card>
-    </Col>
-  ))}
-</Row>
+        <Container>
+          <Row xs={2} md={2} className="g-3">
+            {CardsArr.map((item) => (
+              <Col>
+                <Card style={{ marginTop: '40px' }} className="Cards">
+                  <Card.Img variant="top" src={item.img} />
+                  <Card.Body>
+                    <Card.Title>
+                      <a style={{ textDecoration: 'none', color: 'Black' }} href="">{item.title} </a><br></br>
+                      <a style={{ textDecoration: 'none', color: '#008575' }} href="">{item.desc} </a>
+                    </Card.Title>
 
-</Container>
- 
-      <HomeWrapper>
 
-      <BookCards 
-      catgory = {this.state.catgory} 
-      infoLink={this.state.infoLink}
-      pages={this.state.pages}
-      status ={this.state.status} 
-      author = {this.state.author} 
-      img={this.state.img} 
-      title = {this.state.title} 
-      handleShow={this.handleShow} 
-      show = {this.state.show} 
-      handleClose = {this.handleClose} 
-      favourite={this.props.favourite}  
-      catgories={this.props.catgories} >
 
-      </BookCards>
-      <FilterClassics handleShow={this.handleShow} show = {this.state.show} handleClose = {this.handleClose} favourite={this.favourite}  catgories={this.props.catgories}></FilterClassics>
-      <FilterComics handleShow={this.handleShow} show = {this.state.show} handleClose = {this.handleClose} favourite={this.favourite}  catgories={this.props.catgories}></FilterComics>
-      <FilterMystery handleShow={this.handleShow} show = {this.state.show} handleClose = {this.handleClose} favourite={this.favourite}  catgories={this.props.catgories}></FilterMystery>
-      <FilterHorror handleShow={this.handleShow} show = {this.state.show} handleClose = {this.handleClose} favourite={this.favourite}  catgories={this.props.catgories}></FilterHorror>
-      <FilterLove handleShow={this.handleShow} show = {this.state.show} handleClose = {this.handleClose} favourite={this.favourite}  catgories={this.props.catgories}></FilterLove>
-      </HomeWrapper>
-      
-        
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+
+        </Container>
+
+        <HomeWrapper>
+
+          <BookCards
+            catgory={this.state.catgory}
+            infoLink={this.state.infoLink}
+            pages={this.state.pages}
+            status={this.state.status}
+            author={this.state.author}
+            img={this.state.img}
+            title={this.state.title}
+            handleShow={this.handleShow}
+            show={this.state.show}
+            handleClose={this.handleClose}
+            favourite={this.props.favourite}
+            catgories={this.props.catgories} >
+
+          </BookCards>
+          <FilterClassics
+            catgory={this.state.catgory}
+            infoLink={this.state.infoLink}
+            pages={this.state.pages}
+            status={this.state.status}
+            author={this.state.author}
+            img={this.state.img}
+            title={this.state.title}
+            handleShow={this.handleShow}
+            show={this.state.show}
+            handleClose={this.handleClose}
+            favourite={this.props.favourite}
+            catgories={this.props.catgories}>
+
+          </FilterClassics>
+          <FilterComics
+            catgory={this.state.catgory}
+            infoLink={this.state.infoLink}
+            pages={this.state.pages}
+            status={this.state.status}
+            author={this.state.author}
+            img={this.state.img}
+            title={this.state.title}
+            handleShow={this.handleShow}
+            show={this.state.show}
+            handleClose={this.handleClose}
+            favourite={this.props.favourite}
+            catgories={this.props.catgories}>
+
+          </FilterComics>
+          <FilterMystery
+            catgory={this.state.catgory}
+            infoLink={this.state.infoLink}
+            pages={this.state.pages}
+            status={this.state.status}
+            author={this.state.author}
+            img={this.state.img}
+            title={this.state.title}
+            handleShow={this.handleShow}
+            show={this.state.show}
+            handleClose={this.handleClose}
+            favourite={this.props.favourite}
+            catgories={this.props.catgories}>
+
+          </FilterMystery>
+          <FilterHorror catgory={this.state.catgory}
+            infoLink={this.state.infoLink}
+            pages={this.state.pages}
+            status={this.state.status}
+            author={this.state.author}
+            img={this.state.img}
+            title={this.state.title}
+            handleShow={this.handleShow}
+            show={this.state.show}
+            handleClose={this.handleClose}
+            favourite={this.props.favourite}
+            catgories={this.props.catgories}
+          >
+
+          </FilterHorror>
+          <FilterLove
+            catgory={this.state.catgory}
+            infoLink={this.state.infoLink}
+            pages={this.state.pages}
+            status={this.state.status}
+            author={this.state.author}
+            img={this.state.img}
+            title={this.state.title}
+            handleShow={this.handleShow}
+            show={this.state.show}
+            handleClose={this.handleClose}
+            favourite={this.props.favourite}
+            catgories={this.props.catgories}>
+
+          </FilterLove>
+        </HomeWrapper>
+
+
       </>
     );
   }
